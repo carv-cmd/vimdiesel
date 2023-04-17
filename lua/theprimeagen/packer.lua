a05a6d889e03e6f2b25c2b5d14b912518c59b6a0
wirediq-dev-bc <bcarver@wirediq.com>
2023-04-16 20:17:12 -0400

    Added TODO tree to packer config

--- lua/theprimeagen/packer.lua
+++ lua/theprimeagen/packer.lua
@@ -24,6 +24,18 @@ return require('packer').startup(function(use)
     use('tpope/vim-fugitive')
 
     use {
+        "folke/todo-comments.nvim",
+        requires = "nvim-lua/plenary.nvim",
+        config = function()
+            require("todo-comments").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
+
+    use {
         'VonHeikemen/lsp-zero.nvim',
         branch = 'v2.x',
         requires = {
