95dcbb40ba7e9dce55b536016edce49b35d58425
wirediq-dev-bc <bcarver@wirediq.com>
2023-04-26 17:16:19 -0400

    Added yaml to ensure_installed and enabled indents for python code

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -2,10 +2,13 @@ require'nvim-treesitter.configs'.setup {
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
   -- ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
   -- ensure_installed = { "help", "javascript", "bash", "python", "dockerfile", "perl", "c", "lua", "vim", "vimdoc", "query" },
-  ensure_installed = { "javascript", "markdown", "bash", "python", "dockerfile", "perl", "c", "lua", "vim", "vimdoc", "query" },
+  ensure_installed = { "javascript", "markdown", "bash", "python", "dockerfile", "yaml", "perl", "c", "lua", "vim", "vimdoc", "query" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
+
+  -- Fix bracket indenting in python files
+  indent = { enable = true},
 
   -- Automatically install missing parsers when entering buffer
   -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
