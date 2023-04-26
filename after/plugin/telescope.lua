7cf6c1dc3992a625d83bfe10e370d2fc1f26df3b
wirediq-dev-bc <bcarver@wirediq.com>
2023-04-26 17:15:42 -0400

    Added comments to build on for hidden file search with telescopic johnson

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,7 @@
+-- local telescope = require('telescope')
+-- telescope.setup {pickers = {find_files = {hidden = true}}}
+-- vim.keymap.set("n", '<leader>rg', builtin.find_files, {'rg', '--files', '--iglob', '!.git', '--hidden'})
+
 local builtin = require('telescope.builtin')
 vim.keymap.set("n", '<leader>pf', builtin.find_files, {})
 vim.keymap.set("n", '<C-p>', builtin.git_files, {})
