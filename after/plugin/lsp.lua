0b64e111bd01eab997f52cc1975e4127d63a6f42
wirediq-dev-bc <bcarver@wirediq.com>
2023-04-26 17:14:54 -0400

    Added keymaps in lsp.lua and cleaned up comments

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -2,13 +2,6 @@ local lsp = require('lsp-zero')
 
 lsp.preset("recommended")
 
--- 'tsserver',
--- 'eslint',
--- 'rust_analyzer',
--- 'sumneko_lua',
--- 'lua-language-server',
--- 'awk-language-server',
--- 'bash-language-server',
 lsp.ensure_installed({
   'pyright',
 })
@@ -19,6 +12,7 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
   ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
   ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
   ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+  ['<CR>'] = cmp.mapping.confirm({select = true}),
   ['<C-Space>'] = cmp.mapping.complete(),
 })
 
@@ -43,6 +37,7 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  -- vim.keymap.set("i", "<C-e", function() vim.lsp.options() end, opts)
 end)
 
 -- (Optional) Configure lua language server for neovim
