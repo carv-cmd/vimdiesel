d657f29eeafff623ec6ce24c990cd2f40c27c07d
Ben GigaChad Carver <bencarv1324@gmail.com>
2023-05-06 15:23:44 -0400

    Fixed indenting in treesitter.lua and added more parsers

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,26 +1,42 @@
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  -- ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
-  -- ensure_installed = { "help", "javascript", "bash", "python", "dockerfile", "perl", "c", "lua", "vim", "vimdoc", "query" },
-  ensure_installed = { "javascript", "markdown", "bash", "python", "dockerfile", "yaml", "perl", "c", "lua", "vim", "vimdoc", "query" },
+    -- A list of parser names, or "all" (the five listed parsers should always be installed)
+    -- ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
+    -- ensure_installed = { "help", "javascript", "bash", "python", "dockerfile", "perl", "c", "lua", "vim", "vimdoc", "query" },
+    ensure_installed = {
+        "bash",
+        "python",
+        "perl",
+        "c",
+        "lua",
+        "yaml",
+        "vim",
+        "vimdoc",
+        "html",
+        "css",
+        "javascript",
+        "markdown",
+        "dockerfile",
+        "query"
+    },
 
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-  -- Fix bracket indenting in python files
-  indent = { enable = true},
+    -- Fix bracket indenting in python files
+    indent = {enable = true},
 
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = true,
 
-  highlight = {
-    enable = true,
+    highlight = {
+        enable = true,
 
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
-    additional_vim_regex_highlighting = false,
-  },
+        -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+        -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+        -- Using this option may slow down your editor, and you may see some duplicate highlights.
+        -- Instead of true it can also be a list of languages
+        additional_vim_regex_highlighting = false,
+    },
 }
+
