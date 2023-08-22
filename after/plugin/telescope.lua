e19e5df3519de45cbdacc08d4d02cc4798bb1b61
wirediq-dev-bc <bcarver@wirediq.com>
2023-08-22 17:06:52 -0400

    Added shortcut to open telescope.git_commits

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -8,3 +8,5 @@ vim.keymap.set("n", '<C-p>', builtin.git_files, {})
 vim.keymap.set("n", '<leader>ps', function()
 	builtin.grep_string({ search = vim.fn.input("Grep > ") });
 end)
+vim.keymap.set("n", '<leader>gc', builtin.git_commits, {})
+
