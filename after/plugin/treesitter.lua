87c71b04414a3815d4b797d8c19babc9ac133563
wirediq-dev-bc <bcarver@wirediq.com>
2023-09-27 15:03:47 -0400

    Updated treesitter conf to ignore ssh_config files

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -32,6 +32,9 @@ require'nvim-treesitter.configs'.setup {
 
     highlight = {
         enable = true,
+        disable = {
+            "ssh_config"
+        },
 
         -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
         -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
